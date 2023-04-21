cc233b2a84b1c7bb7a26338aaf72a81ae0ae466a
tinas <ahmettinastepe0@gmail.com>
2023-04-21 15:23:44 +0300

    chore: Add social media links

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import Head from 'next/head'
+import Link from "next/link"
+
+import IconTwitter from 'components/icons/twitter'
+import IconGitHub from 'components/icons/github'
+import IconInstagram from 'components/icons/instagram'
+
+import styles from 'styles/Home.module.scss'
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>Ahmet Tınastepe</title>
         <meta
@@ -11,9 +18,21 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1>Merhaba Ben Ahmet 👋</h1>
+      <main className={styles.main}>
+          <h1 className={styles.hero}>Ahmet Tınastepe</h1>
+          <p className={styles['job-title']}>Software Developer</p>
+          <div className={styles.social}>
+              <Link href="https://github.com/tinas">
+                <IconGitHub className={styles['social__github']}/>
+              </Link>
+              <Link href="https://twitter.com/tinasdev">
+                <IconTwitter className={styles['social__twitter']}/>
+              </Link>
+              <Link href="https://instagram.com/tinasdev">
+                <IconInstagram className={styles['social__instagram']}/>
+              </Link>
+          </div>
       </main>
-    </div>
+    </>
   )
 }
